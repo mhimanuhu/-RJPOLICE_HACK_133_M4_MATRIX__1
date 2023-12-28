ad4435b9daddbda1fce616b1a9fa98fc1498d1f6
mhimanuhu <hacktheworld@gmail.com>
2023-12-28 11:02:52 +0530

    leaderboardupdates

--- src/roles/police/leaderboard/leaderboard.js
+++ src/roles/police/leaderboard/leaderboard.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import "./leaderboard.css";
 import Navbar from '../../../navbar/navbar';
+import './leaderboard.css';
+
 const LeaderboardContainer = styled.div`
   width: 100%;
   max-width: 800px;
@@ -52,52 +53,94 @@ const PoliceStationDetails = styled.div`
   align-items: center;
 `;
 
-const policeStationsData = Array.from({ length: 20 }, (_, index) => ({
-    id: index + 1,
-    name: `Police Station ${index + 1}`,
-    logo: 'rajpolicelogo.png',
-    points: Math.floor(Math.random() * 100) + 100, // Random points between 100 and 199
-  }));
+const citiesOfRajasthan = [
+  "Jaipur",
+  "Udaipur",
+  "Jodhpur",
+  "Ajmer",
+  "Bikaner",
+  "Kota",
+  "Alwar",
+  "Sikar",
+  "Bharatpur",
+  "Pali",
+  "Sawai Madhopur",
+  "Chittorgarh",
+  "Hanumangarh",
+  "Barmer",
+  "Sirohi",
+  "Jhunjhunu",
+  "Dausa",
+  "Tonk",
+  "Nagaur",
+  "Jaisalmer",
+  "Churu",
+  "Bhilwara",
+  "Dungarpur",
+  "Karauli",
+  "Banswara",
+  "Jhalawar",
+  "Dholpur",
+  "Sirohi",
+  "Rajsamand",
+  "Sikar",
+  "Bundi",
+  "Bharatpur",
+  "Baran",
+  "Pratapgarh",
+  "Chittorgarh",
+  "Sri Ganganagar",
+  "Jalore",
+  "Kota",
+  "Chittorgarh",
+  "Dhaulpur",
+  "Bikaner",
+  "Bhilwara",
+  "Banswara",
+  "Baran",
+
+]; // Your array of cities
+
+const policeStationsData = citiesOfRajasthan.map((city, index) => ({
+  id: index + 1,
+  name: `${city} Police Station`,
+  logo: 'rajpolicelogo.png',
+  points: Math.floor(Math.random() * 100) + 100, // Random points between 100 and 199
+}));
 
 const LeaderboardPage = () => {
   const [searchQuery, setSearchQuery] = useState('');
 
-  const filteredPoliceStations = policeStationsData.filter((station) =>
-    station.name.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  const filteredPoliceStations = policeStationsData
+    .filter((station) => station.name.toLowerCase().includes(searchQuery.toLowerCase()))
+    .sort((a, b) => b.points - a.points); // Sort in high to low points
 
   return (
     <>
-    <Navbar pagename="Leaderboard Rankings . . . " />
-    <LeaderboardContainer>
-      <FilterContainer>
-        <SearchInput
-          type="text"
-          placeholder="Search..."
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-      </FilterContainer>
-      <LeaderboardCards>
-        {filteredPoliceStations.map((station) => (
-          <div className="id_container">
-            <div className='logo'>
-                <img src={station.logo} alt={`${station.name} logo `} />
+      <Navbar pagename="Leaderboard Rankings . . . " />
+      <LeaderboardContainer>
+        <FilterContainer>
+          <SearchInput
+            type="text"
+            placeholder="Search..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </FilterContainer>
+        <LeaderboardCards>
+          {filteredPoliceStations.map((station) => (
+            <div className="id_container" key={station.id}>
+              <div className="logo">
+                <img src={station.logo} alt={`${station.name} logo`} />
+              </div>
+              <div className="station_name">{station.name}</div>
+              <div className="station_points">{station.points}</div>
             </div>
-            <div className="station_name">
-                {station.name}
-            </div>
-            <div className="station_points">
-                {station.points}
-            </div>
-          </div>
-        ))}
-      </LeaderboardCards>
-    </LeaderboardContainer>
+          ))}
+        </LeaderboardCards>
+      </LeaderboardContainer>
     </>
   );
 };
 
 export default LeaderboardPage;
-
-
